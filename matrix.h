6a7c1858937838aab5c8ea9e660773945ed34acd
Kasinski Nikita <kasyak2018@gmail.com>
2022-12-07 16:17:22 +0300

    add static method MatrixFactory

--- matrix.h
+++ matrix.h
@@ -5,7 +5,23 @@
 class Matrix
 {
 public:
-    Matrix();
+    enum class MatrixType
+    {
+        Zero,
+        Identity,
+        Translate,
+        Rotate,
+        Scale,
+        ProjectionOXY,
+        ProjectionOXZ,
+        ProjectionOYZ,
+        Aksonometric,
+    };
+    static Matrix MatrixFactory(MatrixType type);
+    void operator*=(Matrix const& other);
+private:
+    Matrix(int n, int m);
+    double **a;
 };
 
 #endif // MATRIX_H
